12c7225c45a14d3b1e90c51aeaa06cd849e6bc14
TotalTaxAmount <shieldscoen@gmail.com>
2023-09-15 13:39:48 -0600

    This is much better

---
+++ dots/hypr/scripts/background.sh
@@ -0,0 +1,17 @@
+export SWWW_TRANSITION_FPS=60
+export SWWW_TRANSITION_STEP=2
+
+# This controls (in seconds) when to switch to the next image
+INTERVAL=60
+swww init
+while true; do
+	find "/home/$USER/.config/swww/wallpapers/" \
+		| while read -r img; do
+			echo "$((RANDOM % 1000)):$img"
+		done \
+		| sort -n | cut -d':' -f2- \
+		| while read -r img; do
+			swww img --transition-type center "$img"
+			sleep $INTERVAL
+		done
+done
