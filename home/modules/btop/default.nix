f91919af589d73f5ad969e6ab843f231a74089fd
TotalTaxAmount <shieldscoen@gmail.com>
2023-09-18 11:45:42 -0600

    btop theme

--- home/modules/btop/default.nix
+++ home/modules/btop/default.nix
@@ -2,7 +2,7 @@
 
 let
   btopTheme = pkgs.substituteAll {
-    src = ../../../dots/btop/themes/theme.conf;
+    src = ../../../dots/btop/themes/system.theme;
     base00 = "#${config.colorScheme.colors.base00}";
     base03 = "#${config.colorScheme.colors.base03}";
     base04 = "#${config.colorScheme.colors.base04}";
@@ -11,12 +11,15 @@ let
     base0C = "#${config.colorScheme.colors.base0C}";
     base0D = "#${config.colorScheme.colors.base0D}";
     base0F = "#${config.colorScheme.colors.base0F}";
-  }
+  };
 in
 {
   programs.btop = {
     enable = true;
+    settings = {
+      color_theme = "system";
+    };
   };
 
-  xdg.configFile."btop/themes/theme.conf".source = btopTheme.out;
+  xdg.configFile."btop/themes/system.theme".source = btopTheme.out;
 }
