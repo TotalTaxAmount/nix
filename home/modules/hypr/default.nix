e5b68660d252d894eac589d454a8cf093faa33ca
TotalTaxAmount <shieldscoen@gmail.com>
2023-09-17 19:07:26 -0600

    I think eww works

--- home/modules/hypr/default.nix
+++ home/modules/hypr/default.nix
@@ -5,13 +5,6 @@ let
     src = ../../../dots/hypr/hyprland.conf;
     base08 = "${config.colorScheme.colors.base08}";
     base03 = "${config.colorScheme.colors.base03}";
-
-    # programs
-    alacritty = "${pkgs.alacritty}";
-    brave = "${pkgs.brave}";
-    rofi = "${pkgs.rofi}";
-    eww = "${pkgs.ewww}";
-    openrgb = "${pkgs.openrgb}";
   };
 in
 {
