6bfb2958fd69c3e9cdfb83e4543ca2bf54ccbe3c
TotalTaxAmount <shieldscoen@gmail.com>
2023-09-18 22:05:58 -0600

    Starting swaylock configs

--- home/modules/swaylock/default.nix
+++ home/modules/swaylock/default.nix
@@ -1,5 +1,16 @@
 { pkgs, config, ...}:
 
+let
+  swaylockConfig = pkgs.substituteAll {
+    src = ../../../dots/swaylock/config;
+    base00 = "${config.colorScheme.colors.base00}";
+    base05 = "${config.colorScheme.colors.base05}";
+    base08 = "${config.colorScheme.colors.base08}";
+
+  };
+in
 {
   home.packages = with pkgs; [ swaylock-effects ];
+
+  xdg.configFile."swaylock/config".source = swaylockConfig.out;
 }
