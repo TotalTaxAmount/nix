fdee0efaf62234c669d783995be9c94683c8b857
TotalTaxAmount <shieldscoen@gmail.com>
2023-09-19 18:09:44 -0600

    Update build.sh for lots of stuff (no way to test if it works...)

--- build.sh
+++ build.sh
@@ -1,6 +1,26 @@
 #! /usr/bin/env bash
 
+# It has to be up here
+set_host() {
+    read -p "Are you sure you want to set your host to $1? (y/n) : " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]];then
+        echo $1 > HOST
+        echo "Done" 
+    else
+        exit 0
+    fi
+}
+
 USER=$(whoami)
+if [ ! -e HOST ];then
+    echo "No host defined"
+    read -p "Enter the name of your host (defined in /outputs/nixos.nix) : " name
+    set_host $name
+fi
+HOST=$(cat HOST)
+
+echo "Current host: $HOST"
 nix_build() {
     case $2 in
         "offline")
@@ -22,15 +42,25 @@ rebuild_home() {
 }
 
 rebuild_system() {
-    sudo nixos-rebuild switch --flake .#laptop
+    sudo nixos-rebuild switch --flake .#$HOST
 }
 
 install_fresh() {
-    read -p "Are you sure? " -n 1 -r
+    echo "Are you sure you are ready"
+    echo "is there a $HOST entry in ./outputs/nixos.conf"
+    echo "and it contains all the correct imports" 
+    echo "(probaly ../system, ../system/hosts/$HOST/configuration.nix & ../system/hosts/$HOST/hardware-configuration.nix)"
+    echo "have you created this hosts directory (/system/hosts/$HOST/)"
+    echo "and created a configuration.nix (not required but recommend)"
+    read -p "If so hit y: " -n 1 -r
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]];then
-        sudo nixos-rebuild switch --experimental-features 'nix-command flakes' --flake .#laptop
+        echo "Copying system scan"
+        cp -r /etc/nixos/hardware-configuration.nix ../system/hosts/$HOST/
+        echo "Building..."
+        sudo nixos-rebuild switch --experimental-features 'nix-command flakes' --flake .#$HOST
         rebuild_home
+        echo "Done"
     fi
     
 }
@@ -43,6 +73,8 @@ case $1 in
         (cd /home/$USER/nix && rebuild_system $2);;
     "fresh")
         (cd /home/$USER/nix && install_fresh);;
+    "set")
+        (cd /home/$USER/nix && set_host $2);;
     *)
-    echo "Options are home, system and fresh"
+    echo "Options are home, system, set and fresh"
 esac
