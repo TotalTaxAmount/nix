874502599691e697689b6a903436ddf3b9a7600e
TotalTaxAmount <shieldscoen@gmail.com>
2023-09-22 17:48:18 -0600

    started xplorer theme

--- home/modules/xplorer/default.nix
+++ home/modules/xplorer/default.nix
@@ -1,5 +1,11 @@
 {pkgs, config, ...}:
 
+let 
+  theme = pkgs.substituteAll {
+    src = "../../../dots/xplorer/systemTheme.xtension";
+    base00 = "#${config.colorScheme.colors.base00}";
+  };
+in 
 {
   home.packages = with pkgs; [xplorer];
 }
