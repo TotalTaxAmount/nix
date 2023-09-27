ddf567b4635c40722ab2602cda4ab5f1c08d736d
TotalTaxAmount <shieldscoen@gmail.com>
2023-09-27 17:39:02 -0600

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,5 +1,30 @@
-# TODO: Make this better
+#!/usr/bin/env bash
 DIR="/home/$USER/nix/shells"
 
-echo "Enabling $1"
-nix-shell $DIR/$1 --run "zsh"
+start_shell() {
+    if [ -z $1 ]; then
+        echo "Error: No shell specified!"
+        echo
+        list_shells
+        exit 1
+    fi
+    echo "Enabling $1"
+    nix-shell $DIR/$1 --run "zsh"
+}
+
+list_shells() {
+    echo "Avalible shells:"
+    for entry in "$DIR"/*;
+    do
+        echo "$(basename $entry)"
+    done
+}
+
+case $1 in
+    "ls")
+        (list_shells);;
+    "s")
+        (start_shell $2);;
+    *)
+    echo "Options are ls and s (start)"
+esac
