8107239124a9a05fe2d26930acab326722b19c38
TotalTaxAmount <shieldscoen@gmail.com>
2023-10-19 13:33:35 -0600

    Forgot to give it color

--- modules/home/remote/default.nix
+++ modules/home/remote/default.nix
@@ -1,1 +1,13 @@
-{}
+{config, inputs, ...}:
+
+let
+
+in {
+  imports = [
+    inputs.nix-colors.homeManagerModule
+  ];
+
+  config = {
+    colorScheme = inputs.nix-colors.colorSchemes.twilight;
+  };
+}
