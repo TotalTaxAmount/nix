32dded6ff3599b09d0d9045c35fba008ea12b593
TotalTaxAmount <shieldscoen@gmail.com>
2024-04-05 12:30:23 -0600

    feature: Start vfio passthough

---
+++ modules/nixos/laptop/vfio/default.nix
@@ -0,0 +1,37 @@
+let
+  # RTX 3070 Ti
+  gpuIDs = [
+    "10de:25a0" # Graphics
+    "10de:2291" # Audio
+  ];
+in { pkgs, lib, config, ... }: {
+  options.vfio.enable = with lib;
+    mkEnableOption "Configure the machine for VFIO";
+
+  config = let cfg = config.vfio;
+  in {
+    boot = {
+      initrd.kernelModules = [
+        "vfio_pci"
+        "vfio"
+        "vfio_iommu_type1"
+        "vfio_virqfd"
+
+        "nvidia"
+        "nvidia_modeset"
+        "nvidia_uvm"
+        "nvidia_drm"
+      ];
+
+      kernelParams = [
+        # enable IOMMU
+        "amd_iommu=on"
+      ] ++ lib.optional cfg.enable
+        # isolate the GPU
+        ("vfio-pci.ids=" + lib.concatStringsSep "," gpuIDs);
+    };
+
+    hardware.opengl.enable = true;
+    virtualisation.spiceUSBRedirection.enable = true;
+  };
+}
