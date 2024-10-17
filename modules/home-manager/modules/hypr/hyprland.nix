0818001745a96d0a07eee9126d3ab3427fa9f950
totaltaxamount <shieldscoen@gmail.com>
2024-10-16 22:15:23 -0600

    update: audio stuff

--- modules/home-manager/modules/hypr/hyprland.nix
+++ modules/home-manager/modules/hypr/hyprland.nix
@@ -54,6 +54,93 @@ let
         exit(1)
   '';
 
+  audioSwitcher = pkgs.writeScriptBin "audioSwitcher" ''
+  #!${pkgs.python3}/bin/python
+  import sys
+  import subprocess
+
+  """
+  Wireplumber sink/source switcher
+
+  It lets you pass your sinks/sources into a dmenu dropdown
+  for ease of access
+
+  Usage:
+  ./wireplumber_audio_switcher.py <Sinks|Sources>
+  """
+
+  GROUP_DELIMITER = " ├─"
+  ITEM_DELIMITER  = " │  "
+  ACCEPTED_GROUPS = set(["Sinks:", "Sources:"])
+
+  def clean_line(line: str):
+      line = line.replace(GROUP_DELIMITER, "").replace(ITEM_DELIMITER, "").replace(":", "")
+      vol_index = line.find("[")
+      if vol_index > 0:
+          line = line[:vol_index]
+      if "*" in line:
+          line = line.replace("*", "")
+          splitted = line.split(".")
+          splitted[1] = f"<b>{splitted[1].strip()} *</b>"
+          line = ". ".join(splitted)
+      return line.strip()
+
+  def parse_wpctl_status():
+      found_audio_tab = False
+      current_subgroup = None
+      processed_data = {}
+      output = subprocess.run(
+          "${pkgs.wireplumber}/bin/wpctl status -k",
+          shell=True,
+          encoding="utf-8",
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE,
+      )
+
+      for line in output.stdout.split("\n"):
+          if not found_audio_tab and line == "Audio":
+              found_audio_tab = True
+
+          elif found_audio_tab:
+              if line == "":
+                  found_audio_tab = False
+                  break
+              elif line == ITEM_DELIMITER:
+                  current_subgroup = None
+                  continue
+              elif line.startswith(GROUP_DELIMITER):
+                  current_subgroup = clean_line(line)
+                  processed_data[current_subgroup] = []
+                  continue
+              elif current_subgroup and line.startswith(ITEM_DELIMITER):
+                  processed_data[current_subgroup].append(clean_line(line))
+                  continue
+      return processed_data
+
+  def pipe_into_dmenu(output):
+      output = subprocess.run(
+          f"echo '{output}' | ${pkgs.rofi}/bin/rofi -dmenu -markup-rows",
+          shell=True,
+          encoding="utf-8",
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE,
+      )
+
+      if output.returncode != 0:
+          return None
+      return output.stdout
+
+  output = parse_wpctl_status()
+  sink = pipe_into_dmenu("\n".join(output[sys.argv[1]]))
+
+  if sink:
+      sink_id = sink.split(".")[0]
+      subprocess.run(
+          f"${pkgs.wireplumber}/bin/wpctl set-default {sink_id}",
+          shell=True
+      )
+  '';
+
   baseConfig = builtins.readFile ../../../../dots/hypr/hyprland/hyprland.conf;
   laptopExtra = builtins.readFile ../../../../dots/hypr/hyprland/laptopExtra.conf;
   laptopStrixExtra = builtins.readFile ../../../../dots/hypr/hyprland/laptopStrixExtra.conf;
@@ -72,6 +159,7 @@ let
     base0D = "${config.colorScheme.palette.base0D}";
     user = "${user}";
     backgrounds = "${backgrounds}/bin/backgrounds";
+    audioSwitcher = "${audioSwitcher}/bin/audioSwitcher";
     cursorSize = config.cursor.size;
     cursorTheme = config.cursor.name;
   };
@@ -80,6 +168,7 @@ in
   home.packages = with pkgs; [
     wlr-randr
     backgrounds
+    audioSwitcher
     hyprland-activewindow
   ];
 
